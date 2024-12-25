6e147ed1ad0b9a4e6fa20f20b73d4f48a4b22b55
sudipsudip5250 <sudipsudip5250@gmail.com>
2024-12-25 20:51:26 +0530

    first code added with necessary files and image

---
+++ script.js
@@ -0,0 +1,17 @@
+const a = document.getElementById('img')
+
+function myfanon(){
+    a.style.animationDuration = 3 + 's';
+}
+function myfanoff(){
+    a.style.animationDuration = 0 + 's';
+}
+function myfan1(){
+    a.style.animationDuration = 1 + 's';
+}
+function myfan2(){
+    a.style.animationDuration = 0.5 + 's';
+}
+function myfan3(){
+    a.style.animationDuration = 0.2 + 's';
+}
